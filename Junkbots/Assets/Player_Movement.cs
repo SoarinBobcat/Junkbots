a3d18ca549f8513a7a9f9e7650eeb4553b597b69
SoarinBobcat <125240893+SoarinBobcat@users.noreply.github.com>
2023-03-29 13:48:28 +0800

    schmovement

---
+++ Junkbots/Assets/Player_Movement.cs
@@ -0,0 +1,98 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Movement : MonoBehaviour
+{
+    //Player Speed Variables
+    int spd = 10;
+    float acc = 0.1f;
+    float decc = 0.3f;
+
+    float k_forward;
+    float k_backward;
+    float k_left;
+    float k_right;
+    float x_move;
+    float z_move;
+    Vector3 dir = Vector3.zero;
+    Vector3 currSpd = Vector3.zero;
+
+    //Player Jump Variables
+    int grav = 1;
+    int jump_spd = 12;
+
+    //State Machine stuff for later ;)
+    enum STATES
+    {
+        Main,
+        Attack,
+        AltAttack,
+        Dodge,
+        Hurt
+    }
+
+    int s = (int) STATES.Main;
+
+    private CharacterController c_c;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        c_c = gameObject.GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get Player Input
+        k_forward = Input.GetKey(KeyCode.W) ? 1 : 0;
+        k_backward = Input.GetKey(KeyCode.S) ? 1 : 0;
+        k_left = Input.GetKey(KeyCode.A) ? 1 : 0;
+        k_right = Input.GetKey(KeyCode.D) ? 1 : 0;
+
+        //Convert Input to Axes
+        x_move = k_right-k_left;
+        z_move = k_forward-k_backward;
+
+        //Convert Axes to Direction Vector
+        dir = new Vector3(x_move, 0, z_move);
+        dir = Vector3.Normalize(dir);
+
+        //Add acceleration to current velocity...or decceleration if no meowvement =|owo|=
+        if ((Mathf.Abs(x_move) == 1)) {
+            currSpd.x += dir.x * acc;
+        }
+        else
+        {
+            if ((Mathf.Abs(currSpd.x) - Mathf.Sign(currSpd.x) * decc) > 0)
+            {
+                currSpd.x -= Mathf.Sign(currSpd.x) * decc;
+            }
+            else
+            {
+                currSpd.x = 0;
+            }
+        }
+
+        if ((Mathf.Abs(z_move) == 1)) {
+            currSpd.z += dir.z * acc;
+        } else
+        {
+            if ((Mathf.Abs(currSpd.z) - Mathf.Sign(currSpd.z) * decc) > 0)
+            {
+                currSpd.z -= Mathf.Sign(currSpd.z) * decc;
+            }
+            else
+            {
+                currSpd.z = 0;
+            }
+        }
+
+        currSpd.x = Mathf.Clamp(currSpd.x, -spd, spd);
+        currSpd.z = Mathf.Clamp(currSpd.z, -spd, spd);
+
+        //Apply velocity
+        c_c.Move(currSpd * Time.deltaTime);
+    }
+}
