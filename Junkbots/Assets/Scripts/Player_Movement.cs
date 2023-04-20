2c4b0cd683ac255b58695f7524a4597d92f77b12
SoarinBobcat <30073280@tafe.wa.edu.au>
2023-04-21 06:48:41 +0800

    Movement Fixed for real this time I swear :wink:
    Should be all good now :)

--- Junkbots/Assets/Scripts/Player_Movement.cs
+++ Junkbots/Assets/Scripts/Player_Movement.cs
@@ -4,27 +4,46 @@ using UnityEngine;
 
 public class Player_Movement : MonoBehaviour
 {
-    //Player Speed Variables
-    int spd = 8;
-    float g_acc = 0.3f;
-    float a_acc = 0.05f;
-    float decc = 0.2f;
+    //Movment Variable Class Set Up
+    [System.Serializable]
+    public class MovementSettings
+    {
+        public float Spd;
+        public float Acc;
+        public float Decc;
 
-    float x_move;
-    float z_move;
+        public MovementSettings(float spd, float acc, float decc)
+        {
+            Spd = spd;
+            Acc = acc;
+            Decc = decc;
+        }
+    }
+
+    //Player Speed Variables
+    [Header("Movement")]
+    [SerializeField] private MovementSettings gSettings = new MovementSettings(20, 1, 0.5f);
+    [SerializeField] private MovementSettings aSettings = new MovementSettings(20, 0.5f, 0.5f);
+    [SerializeField] private float fric = 0.1f;
+
+    //Player Jump Variables
+    [SerializeField] private float grav = 1f;
+    [SerializeField] private float jumpSpd = 20f;
+
+    //Input Variables
     Vector3 xMove;
     Vector3 zMove;
     Vector3 dir = Vector3.zero;
-    Vector3 currSpd = Vector3.zero;
+
+    //Current Velocity Variables
+    float currSpd = 0f;
+    Vector3 vel = Vector3.zero;
+    float accel = 0;
 
     //Turning Variables
     float turnSmoothTime = 0.1f;
     float turnSmoothVelocity;
     float targetAngle = 0;
-
-    //Player Jump Variables
-    float grav = 0.2f;
-    int jumpSpd = 20;
 
     //State Machine stuff for later ;)
     enum STATES
@@ -51,91 +70,100 @@ public class Player_Movement : MonoBehaviour
     void Update()
     {
         //Get Player Input
-        Vector3 camForward = cam.forward;
-        Vector3 camRight = cam.right;
-
-        camForward.y = 0;
-        camRight.y = 0;
-
-        camForward = camForward.normalized;
-        camRight = camRight.normalized;
-
-        x_move = Input.GetAxisRaw("Horizontal");
-        z_move = Input.GetAxisRaw("Vertical");
-
-        xMove = x_move * cam.right;
-        zMove = z_move * cam.forward;
+        xMove = Input.GetAxisRaw("Horizontal") * cam.right;
+        zMove = Input.GetAxisRaw("Vertical") * cam.forward;
 
         dir = xMove + zMove;
+        dir.y = 0;
+        dir.Normalize();
 
         //Turn to face movement dir
-        if ((x_move != 0) || (z_move != 0))
+        if ((Input.GetAxisRaw("Horizontal") != 0) || (Input.GetAxisRaw("Vertical") != 0))
         {
             targetAngle = Mathf.Atan2(dir.x, dir.z) * Mathf.Rad2Deg;
         }
         float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
         transform.rotation = Quaternion.Euler(0f, angle, 0f);
 
-        //Add acceleration to current velocity...or decceleration if no meowvement =|owo|=
-        if ((x_move != 0) || (z_move != 0))
-        {
-            if (c_c.isGrounded)
-            {
-                currSpd += xMove * g_acc + zMove * g_acc;
-            }
-            else
-            {
-                currSpd += xMove * a_acc + zMove * a_acc;
-            }
-        } 
-        
+        //Movement
         if (c_c.isGrounded)
         {
-            currSpd.x = deccelerate(currSpd.x);
-            currSpd.z = deccelerate(currSpd.z);
+            GroundMove();
         }
-
-        //Clamp speed
-        currSpd.x = Mathf.Clamp(currSpd.x, -spd, spd);
-        currSpd.z = Mathf.Clamp(currSpd.z, -spd, spd);
-
-        //Jumping "Yahoo~♪"
-        //Apply gravity when in air
-        if (!c_c.isGrounded){
-            currSpd.y -= grav;
-        } 
         else
         {
-            currSpd.y = -1f;
+            AirMove();
         }
+
+        //Apply velocity
+        c_c.Move(vel * Time.deltaTime);
+    }
+
+    //Handles Ground Movement
+    private void GroundMove()
+    {
+        Friction();
+
+        //Check to see if the player is trying to move opposite to their current velocity
+        if (Vector3.Dot(vel, dir) < 0)
+        {
+            accel = gSettings.Decc;
+        }
+        else
+        {
+            accel = gSettings.Acc;
+        }
+
+        //Jumping "Yahoo~♪"
+        vel.y = -1f;
 
         //Jump if grounded
         if (Input.GetKeyDown(KeyCode.Space) && c_c.isGrounded)
         {
-            currSpd.y += jumpSpd;
+            vel.y += jumpSpd;
         }
 
-        //Apply velocity
-        c_c.Move(currSpd * Time.deltaTime);
+        //Apply Accelearation and Clamp Speed
+        vel.x = Mathf.Clamp((vel.x + (dir.x * accel)), -gSettings.Spd, gSettings.Spd);
+        vel.z = Mathf.Clamp((vel.z + (dir.z * accel)), -gSettings.Spd, gSettings.Spd);
     }
 
-    /*
-    Calculates the acceleration and decceleration of an axis;
-    d = dir.[The axis to be calculated]
-    vel = currSpd.[The axis to be calculated]*/
-    
-    private float deccelerate(float vel)
+    //Applies Friction
+    private void Friction()
     {
-      
-        if ((Mathf.Abs(vel) - Mathf.Sign(vel) * decc) > 0)
+        Vector3 velocity = vel;
+        velocity.y = 0;
+        float speed = velocity.magnitude;
+        float drop = 0;
+
+        float control = speed < gSettings.Decc ? gSettings.Decc : speed;
+        drop = control * fric;
+
+        float newSpeed = speed - drop;
+        if (newSpeed < 0)
         {
-            vel -= Mathf.Sign(vel) * decc;
-        }
-        else
-        {
-            vel = 0;
+            newSpeed = 0;
         }
 
-        return vel;
+        if (newSpeed > 0)
+        {
+            newSpeed /= speed;
+        }
+
+        vel.x *= newSpeed;
+        vel.z *= newSpeed;
+    }
+
+    //Handles Air Movement
+    private void AirMove()
+    {
+        //Apply Gravity
+        vel.y -= grav;
+
+        //Apply Accelearation and Clamp Speed
+        accel = aSettings.Acc;
+
+        vel.x = Mathf.Clamp((vel.x + (dir.x * accel)), -gSettings.Spd, gSettings.Spd);
+        vel.z = Mathf.Clamp((vel.z + (dir.z * accel)), -gSettings.Spd, gSettings.Spd);
     }
 }
